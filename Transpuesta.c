89ddb1f5d14e321c395058744aace5ef1e1e341d
Rodrigo Andrade <rodrigo.andrade@udla.edu.ec>
2023-11-23 16:31:17 -0500

    Se agregan comentarios

--- Transpuesta.c
+++ Transpuesta.c
@@ -1,25 +1,33 @@
+//Librerias
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
+//Inicio del programa
 int main (int argc, char const *argv[])
 {
+    //Entrada de variables e impresiones de entrada
     int n , m; 
     printf("Ingrese filas: ");
     scanf("%d", &n);    
     printf("Ingrese columas: ");
     scanf("%d", &m);
+    //Instruccion aletoria
     srand (time(NULL));
+    //Entrada de resulado 1 e impresion de titulo de matriz original
     int Matriz[n][m];
     printf("Matriz Original: \n"); 
-        for (int i = 0; i < n; i++)
-    {       
+    //Inicio de funciones 
+    for (int i = 0; i < n; i++)
+    {          
         printf("\n");        
-        for(int j = 0; j < m; j++){    
+        for(int j = 0; j < m; j++){ 
+            //Calculos necesarios para numeros aletorios de 0 a 100 e impresion de dichos numeros en la matriz original   
             Matriz[i][j]=rand()%101;; 
             printf("%d\t", Matriz[i][j]);                       
         }
     }
+    //Entrada de resulado 2 e impresion de titulo de matriz transpuesta
     int Matriz2[m][n];   
     printf("\n"); 
     printf("\nMatriz Transpuesta: \n \n");     
@@ -27,6 +35,7 @@ int main (int argc, char const *argv[])
     for (int i = 0; i < n; i++)
     {               
         for(int j = 0; j < m; j++){  
+            //Calculos necesarios la matriz transpuesta
             Matriz2[i][j]=Matriz[j][i];          
             printf("%d\t", Matriz2[i][j]);                                             
         }
